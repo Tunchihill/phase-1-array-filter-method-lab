d295219b2ebd70364a72fd9682be322649e02979
Arnold Kiprono Langat <arnoldkiprono3@gmail.com>
2024-04-11 14:41:30 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(arr, target) {
+    return arr.filter(
+      (item) => item.toLowerCase() === target.toLowerCase()
+    );
+  }
+  function fuzzyMatch(sourceArr, testStr) {
+    return sourceArr.filter(
+      (item) =>
+        item.toLowerCase().indexOf(testStr.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(dataArr, targetName) {
+    return dataArr.filter((entry) => entry.name === targetName);
+  }
+  
